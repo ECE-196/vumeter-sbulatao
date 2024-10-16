e0e10d2124ab80d25aa98714a84226175122ac24
sbulatao <114209345+sbulatao@users.noreply.github.com>
2024-10-16 15:30:16 -0700

    added code

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ led_pins = [
     board.IO36,
     board.IO37,
     board.IO38,
-    board.IO49
+    board.IO39
 ]
 
 leds = [DigitalInOut(pin) for pin in led_pins]
@@ -28,16 +28,30 @@ leds = [DigitalInOut(pin) for pin in led_pins]
 for led in leds:
     led.direction = Direction.OUTPUT
 
+
+# Helper function that uses microphone value to light up LED
+def lightUp(volume, maxVolume, numberLeds):
+    lighting = maxVolume/numberLeds
+    return min(numberLeds, int(volume/lighting))
+    # return int ((volume/maxVolume) * numberLeds)
+
+
 # main loop
 while True:
     volume = microphone.value
 
     print(volume)
 
+    ledsOn = lightUp(volume, 45000, len(leds)) # lights up led based on microphone
+
+    # Turning on leds
+    for i in range(len(leds)):
+        leds[i].value = i < ledsOn # This should turn leds on
+
     leds[0].value = not leds[0].value
     leds[1].value = not leds[0].value
 
-    sleep(1)
+    sleep(0.1)
 
     # instead of blinking,
     # how can you make the LEDs
